f86960b806daaaae813338639442648a8afb19a3
Adiat <adiat4them@gmail.com>
2019-10-30 22:10:22 +0000

    created uncompleted list_page.hml page for clothing app

--- clothing/urls.py
+++ clothing/urls.py
@@ -4,4 +4,5 @@ from clothing import views
 app_name = "clothing"
 urlpatterns = [
     path('', views.index_page, name='index'),
+    path('all-cloth/', views.cloth_list, name='cloth_list')
 ]
--- clothing/views.py
+++ clothing/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'clothing/index_page.html')
+def cloth_list(request):
+    return render(request, 'clothing/list_page.html')
