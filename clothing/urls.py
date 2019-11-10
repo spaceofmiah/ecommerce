4adfbe76c93c1e41f22d8d2e85a33c1b7825e74d
spaceofmiah <geek_200@outlook.com>
2019-11-10 18:26:10 +0100

    refactor code ::: <cleanup codebase>

--- clothing/urls.py
+++ clothing/urls.py
@@ -3,9 +3,34 @@ from clothing import views
 
 app_name = "clothing"
 urlpatterns = [
-    path('', views.index_page, name='index'),
-    path('all-cloth/', views.cloth_list, name='cloth_list'),  
-    path('search-cloth/', views.handle_cloth_search, 
-                                        name='handle_cloth_search'),
-    path('create-cloth', views.handle_cloth_creation, name='create_cloth'),
+    path(
+            '', 
+            views.index_page, 
+            name='index'
+        ),
+
+    path(
+            'all-cloth/', 
+            views.cloth_list, 
+            name='cloth_list'
+        ),  
+
+    path(
+            'search-cloth/', 
+            views.handle_cloth_search, 
+            name='handle_cloth_search'
+        ),
+
+    path(
+            'create-cloth', 
+            views.handle_cloth_creation, 
+            name='create_cloth'
+        ),
+    
+    path(
+            'cart/<int:cloth_id>/', 
+            views.handle_add_to_cart, 
+            name='add_to_cart'
+        ),
+
 ]
