5b7b4c18a1f033b39397e33e6474bd41b6185e50
spaceofmiah <geek_200@outlook.com>
2019-11-23 13:19:54 +0100

    Integrate django-bootstrap4, use it on cloth create & update form

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clothing'
+    'clothing',
+
+    #third party library
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
