2ceed3205132f5a1c081d74dd9054d1f393a1b4b
spaceofmiah <geek_200@outlook.com>
2019-11-23 13:56:06 +0100

    Create CartItem model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from clothing.models import Cloth
+
+class CartItem(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    # a ForeignKey is unique by default, making it the other
+    # way round allows it to hold more than one object of 
+    # same kind. An online cart cannot vitually hold two of 
+    # same object unlike our basket when we go to grocery 
+    # store, and that is why if a product of same kind that
+    # already exists in our cart is to be readded, we'll just
+    # increment the quantity property of the existing product
+    # hence our quantity field below
+    product = models.ForeignKey(
+        Cloth,
+        related_name="+",
+        on_delete = models.CASCADE,
+        unique=False        # this here does the trick
+    )
+
+    quantity = models.IntegerField(default=1)
+
+
+    class Meta:
+        verbose_name_plural = 'Cart Item'
+        verbose_name = 'Cart Item'
+
+
+
+    ### Helper Methods ###
+
+    def __str__(self):
+        """
+        returns string representation of objects
+        """
+        return self.product.name
+
+    def get_absolute_url(self):
+        """
+        returns the actual url of the cart item
+        """
+        return self.product.get_absolute_url()
+
+    def augment_quantity(self):
+        """
+        Increase the quantity of an existing cart item by 1
+        if the item is to be added again
+        """
+        self.quantity += 1
+        self.save()
+
+
+    def total_price(self):
+        """
+        calculates the total price of a cart item
+        :: cart_item_price * quantity_added_to_cart
+        """
+        return self.product.price * self.quantity
