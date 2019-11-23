c2925dc6f0388e08c70f1651558b728605ad4881
Adiat <adiat4them@gmail.com>
2019-11-23 22:11:51 +0000

    create cart model and test on terminal

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from clothing.models import Cloth
 
 # Create your models here.
+class CartItem(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    product = models.ForeignKey(
+        Cloth,
+        related_name="+",
+        on_delete = models.CASCADE
+    )
+
+    quantity = models.IntegerField(default=1)
+
+    def ___str___(self):
+        return self.product.name + "cloth item"
+
