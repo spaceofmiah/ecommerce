f67ca3fb6b93aab4a070d6ea3c5ff670b271ab92
spaceofmiah <geek_200@outlook.com>
2019-11-24 21:22:09 +0100

    Refactor add_to_cart helper class

--- cart/helpers.py
+++ cart/helpers.py
@@ -6,6 +6,10 @@ from cart.models import CartItem, Cart
 
 
 def _create_cart():
+    """
+    : private access
+    create and returns a cart object
+    """
     cart = Cart(ticket = str(uuid.uuid4()))
     cart.save()
     return cart
@@ -13,7 +17,7 @@ def _create_cart():
 
 def add_to_cart(request, product):
     """
-    adds an item with the above id to cart and increments
+    adds a product to cart and increments
     the quantity of the item when it's already present in 
     cart
 
@@ -21,17 +25,27 @@ def add_to_cart(request, product):
     : product -- the product to be added to cart
     """
     # - get the cart id of the requesting user from session
-    # an if non is found create one for the user
+    cart_id = int(request.session['cart'])
 
     # - retrieve the cart from the Cart object
+    cart = get_object_or_404(Cart, pk=cart_id)
 
-    # - check if the item whose id is passed is already present 
-    # within the retrieved cart of the requesting user
+    # - check if passed product  is already present  within the 
+    # retrieved cart of the requesting user
+
+   
+    # check if the id of cart items underlying id and product
+    # to be added id are same
+    if CartItem.objects.filter(product__id=product.id).exists():
+        cart_item = CartItem.objects.get(product__id=product.id)
+        # if same, then increase the cart item quanity instead
+        cart_item.augment_quantity()
+    else:
+        # add the product
+        cart_item = _create_cart_item_helper(product)
+        cart.items.add(cart_item)
     
-    # - if present, increase the quantity by one
-          
-    # - create new cart item and include to cart
-    pass
+    return cart
 
 
 
@@ -40,10 +54,13 @@ def add_to_cart(request, product):
 
 def _create_cart_item_helper(product):
     """
+    : private access
     creates and return a new cart item
 
     : product -- this is the product that is to be
     added to cart
     """
-    return CartItem.objects.create(product = product)
+    cart_item = CartItem.objects.create(product = product)
+    print(f"{cart_item} created sucessfully")
+    return cart_item
 
