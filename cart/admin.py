bbfdb79672a98f3c3877d691aba35a6bd4f29f3e
spaceofmiah <geek_200@outlook.com>
2019-11-24 21:23:14 +0100

    Update admin.py with conditional to include carts DefaultProduct

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from cart.models import CartItem, Cart
+from django.conf import settings
+from cart.models import CartItem, Cart, DefaultProduct
 
+DISPLAY_DEFAULT_CART_PRODUCT = getattr(settings, 'DISPLAY_DEFAULT_CART_PRODUCT', False)
+
+if DISPLAY_DEFAULT_CART_PRODUCT:
+    admin.site.register(DefaultProduct)
+    
 admin.site.register(CartItem)
 admin.site.register(Cart)
